834935ecefd5aaee3450bf7c0a02adadb9ce3fb0
Eric P <pericappstudio@gmail.com>
2016-04-02 12:12:51 -0500

    Added Splash Activity.
    Credit goes to RedbirdHacks for the feather.

---
+++ app/src/main/java/com/ercpng/redbirdhacks2016/activities/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ercpng.redbirdhacks2016.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
