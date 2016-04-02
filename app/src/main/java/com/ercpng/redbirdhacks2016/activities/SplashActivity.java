2426b0c771a65eb56cebe3ba7aa969917d4348ae
Eric P <pericappstudio@gmail.com>
2016-04-02 13:10:36 -0500

    Added general layout of the game.

--- app/src/main/java/com/ercpng/redbirdhacks2016/activities/SplashActivity.java
+++ app/src/main/java/com/ercpng/redbirdhacks2016/activities/SplashActivity.java
@@ -1,27 +1,22 @@
 package com.ercpng.redbirdhacks2016.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.ercpng.redbirdhacks2016.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private TextView tv;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        tv = (TextView) findViewById(R.id.textView);
     }
 
 
     public void makeToast(View view) {
-        Toast.makeText(getApplicationContext(), "Going to test", Toast.LENGTH_LONG).show();
-        tv.setText("Changed text");
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 }
